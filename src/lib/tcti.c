70cb2dbd577c03f5decbc0b7b0dc18ebbe45e450
Yunguo Wei <yunguo.wei@windriver.com>
2018-09-07 10:57:28 +0800

    cryptfs-tpm2: support new API of tpm2-tss 2.x LIB
    This commit is adapting the following changes in tpm2-tss 2.x::
    1. Renamed shared libraries
    2. New TCTI init API.
    
    Signed-off-by: Yunguo Wei <yunguo.wei@windriver.com>

--- src/lib/tcti.c
+++ src/lib/tcti.c
@@ -40,22 +40,37 @@ init_tcti_tabrmd(void)
 {
 	TSS2_TCTI_CONTEXT *ctx;
 	size_t size;
-	TSS2_RC (*init)(TSS2_TCTI_CONTEXT *, size_t *);
 	TSS2_RC rc;
+#ifndef TSS2_LEGACY_V1
+	TSS2_RC (*init)(TSS2_TCTI_CONTEXT *, size_t *, const char *);
+
+	tcti_handle = dlopen("libtss2-tcti-tabrmd.so.0", RTLD_LAZY);
+#else
+	TSS2_RC (*init)(TSS2_TCTI_CONTEXT *, size_t *);
 
 	tcti_handle = dlopen("libtcti-tabrmd.so.0", RTLD_LAZY);
+#endif
+
 	if (!tcti_handle) {
 		err("Unable to find out the tabrmd tcti library\n");
 		return NULL;
 	}
 
+#ifndef TSS2_LEGACY_V1
+	init = dlsym(tcti_handle, "Tss2_Tcti_Tabrmd_Init");
+#else
 	init = dlsym(tcti_handle, "tss2_tcti_tabrmd_init");
+#endif
 	if (!init) {
 		dlclose(tcti_handle);
 		return NULL;
 	}
 
+#ifndef TSS2_LEGACY_V1
+	rc = init(NULL, &size, NULL);
+#else
 	rc = init(NULL, &size);
+#endif
 	if (rc != TSS2_RC_SUCCESS) {
 		dlclose(tcti_handle);
 		err("Unable to get the size of tabrmd tcti context\n");
@@ -64,7 +79,11 @@ init_tcti_tabrmd(void)
 
 	ctx = (TSS2_TCTI_CONTEXT *)malloc(size);
 	if (ctx) {
+#ifndef TSS2_LEGACY_V1
+		rc = init(ctx, &size, NULL);
+#else
 		rc = init(ctx, &size);
+#endif
 		if (rc != TSS2_RC_SUCCESS) {
 			err("Unable to initialize tabrmd tcti context\n");
 			free(ctx);
@@ -79,16 +98,24 @@ init_tcti_tabrmd(void)
 TSS2_TCTI_CONTEXT *
 init_tcti_device(void)
 {
+#ifndef TSS2_LEGACY_V1
+	const char *cfg = "/dev/tpm0";
+#else
 	TCTI_DEVICE_CONF cfg = {
 		.device_path = "/dev/tpm0",
 		.logCallback = NULL,
 		.logData  = NULL,
 	};
+#endif
 	size_t size;
 	TSS2_TCTI_CONTEXT *ctx;
 	TSS2_RC rc;
 
+#ifndef TSS2_LEGACY_V1
+	rc = Tss2_Tcti_Device_Init(NULL, &size, NULL);
+#else
 	rc = InitDeviceTcti(NULL, &size, NULL);
+#endif
 	if (rc != TSS2_RC_SUCCESS) {
 		err("Unable to get the size of device tcti context\n");
 		return NULL;
@@ -96,7 +123,11 @@ init_tcti_device(void)
 
 	ctx = (TSS2_TCTI_CONTEXT *)malloc(size);
 	if (ctx) {
+#ifndef TSS2_LEGACY_V1
+		rc = Tss2_Tcti_Device_Init(ctx, &size, cfg);
+#else
 		rc = InitDeviceTcti(ctx, &size, &cfg);
+#endif
 		if (rc != TSS2_RC_SUCCESS) {
 			err("Unable to initialize device tcti context\n");
 			free(ctx);
@@ -110,6 +141,9 @@ init_tcti_device(void)
 TSS2_TCTI_CONTEXT *
 init_tcti_socket(void)
 {
+#ifndef TSS2_LEGACY_V1
+	const char *cfg = "host=127.0.0.1,port=2321";
+#else
 	TCTI_SOCKET_CONF cfg = {
 		.hostname = DEFAULT_HOSTNAME,
 		.port = 2321,
@@ -117,11 +151,16 @@ init_tcti_socket(void)
 		.logBufferCallback = NULL,
 		.logData = NULL,
 	};
+#endif
 	size_t size;
 	TSS2_TCTI_CONTEXT *ctx;
 	TSS2_RC rc;
 
+#ifndef TSS2_LEGACY_V1
+	rc = Tss2_Tcti_Mssim_Init(NULL, &size, cfg);
+#else
 	rc = InitSocketTcti(NULL, &size, &cfg, 0);
+#endif
 	if (rc != TSS2_RC_SUCCESS) {
 		err("Unable to get the size of socket tcti context\n");
 		return NULL;
@@ -129,7 +168,11 @@ init_tcti_socket(void)
 
 	ctx = (TSS2_TCTI_CONTEXT *)malloc(size);
 	if (ctx) {
+#ifndef TSS2_LEGACY_V1
+		rc = Tss2_Tcti_Mssim_Init(ctx, &size, cfg);
+#else
 		rc = InitSocketTcti(ctx, &size, &cfg, 0);
+#endif
 		if (rc != TSS2_RC_SUCCESS) {
 			err("Unable to initialize socket tcti context\n");
 			free(ctx);
@@ -167,7 +210,11 @@ cryptfs_tpm2_tcti_init_context(void)
 void
 cryptfs_tpm2_tcti_teardown_context(TSS2_TCTI_CONTEXT *ctx)
 {
+#ifndef TSS2_LEGACY_V1
+	Tss2_Tcti_Finalize(ctx);
+#else
 	tss2_tcti_finalize(ctx);
+#endif
 	free(ctx);
 
 	if (tcti_handle)
