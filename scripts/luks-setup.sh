7b9ae9b96ac8a02600b09f848e81ef0fbd7061fd
Jia Zhang <zhang.jia@linux.alibaba.com>
2024-05-15 19:12:34 +0800

    Refine luks-setup.sh
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

--- scripts/luks-setup.sh
+++ scripts/luks-setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# The wrapper script for the creation of LUKS partition
+# The wrapper script for the creation of LUKS volume
 #
-# Copyright (c) 2016-2017, Wind River Systems, Inc.
+# Copyright (c) 2024, Alibaba Cloud
+# Copyright (c) 2016-2013, Wind River Systems, Inc.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -35,14 +36,14 @@
 #
 
 # Use example:
-#   luks-setup.sh -d /dev/sdb1 -n my_luks_part
+#   luks-setup.sh -d /dev/sdb1 -n my_luks_volume
 
 VERSION="0.1.0"
 
 # If the luks-setup.sh is called to map a drive before the boot is completed
 # manage the resource manager here as needed
 RESOURCEMGR_STARTED=0
-DEFAULT_ENCRYPTION_NAME="${DEFAULT_ENCRYPTION_NAME:-luks_part}"
+DEFAULT_ENCRYPTION_NAME="${DEFAULT_ENCRYPTION_NAME:-luks_volume}"
 TPM_ABSENT=1
 TEMP_DIR=""
 
@@ -80,10 +81,19 @@ print_verbose() {
 
 # Remove the sensitive passphrase in case accidentally terminated
 trap_handler() {
+    local line_no="$1"
+    local err="$2"
+
     print_verbose "Cleaning up ..."
     [ $RESOURCEMGR_STARTED -eq 1 ] && pkill tpm2-abrmd
     [ -n "$TEMP_DIR" ] && rm -rf "$TEMP_DIR"
     unset TPM2TOOLS_DEVICE_FILE TPM2TOOLS_TCTI_NAME TSS2_TCTI
+
+    if [ $err -ne 0 ] ; then
+        print_error "Error occurred on line $line_no, exit code: $err"
+    fi
+
+    exit $err
 }
 
 detect_tpm() {
@@ -105,25 +115,31 @@ detect_tpm() {
         grep -q "TPM 2.0 Device" "/sys/class/tpm/$dev/device/firmware_node/description" 2>/dev/null &&
             tpm_absent=0 && break
 
+	# With the latest kernel, use tpm_version_major
+	grep -q "2" "/sys/class/tpm/$dev/tpm_version_major" 2>/dev/null &&
+	    tpm_absent=0 && break
+
 	# Support virtual TPM
-	ls "/sys/class/tpm/$dev/device/driver" 2> /dev/null | grep -q MSFT0101 && tpm_absent=0 && break
+	ls "/sys/class/tpm/$dev/device/driver" 2>/dev/null | grep -q MSFT0101 && tpm_absent=0 && break
     done
 
-    [ $tpm_absent -eq 1 ] && print_info "No TPM device found" && return 1
+    [ $tpm_absent -eq 1 ] && print_info "No TPM 2.0 device found" && return 1
 
     pgrep tpm2-abrmd >/dev/null
     [ $? -ne 0 ] && {
         TPM2TOOLS_TCTI_NAME=device
-        export TPM2TOOLS_DEVICE_FILE=/dev/tpm0
+        TPM2TOOLS_TCTI=device
+        export TPM2TOOLS_DEVICE_FILE=/dev/tpmrm0
         TSS2_TCTI=device
     } || {
         TPM2TOOLS_TCTI_NAME=abrmd
+        TPM2TOOLS_TCTI=abrmd
         TSS2_TCTI=tabrmd
     }
 
-    export TPM2TOOLS_TCTI_NAME TSS2_TCTI
+    export TPM2TOOLS_TCTI_NAME TSS2_TCTI TPM2TOOLS_TCTI
 
-    print_info "TPM device /dev/$dev detected"
+    print_info "TPM 2.0 device $dev detected"
 
     return 0
 }
@@ -131,6 +147,7 @@ detect_tpm() {
 unseal_passphrase() {
     local passphrase=$1
     local err=0
+    local pcr_opt=""
 
     if [ x"$TPM2TOOLS_TCTI_NAME" = x"abrmd" ]; then
         RESOURCEMGR_STARTED=1
@@ -139,7 +156,11 @@ unseal_passphrase() {
     fi
 
     if [ $err -eq 0 ]; then
-        ! cryptfs-tpm2 -q unseal passphrase -P auto -o "$passphrase" &&
+        if [ $OPT_USE_PCR -eq 1 ]; then
+            pcr_opt="-P auto"
+        fi
+
+        ! cryptfs-tpm2 -q unseal passphrase $pcr_opt -o "$passphrase" &&
 	    print_error "Unable to unseal the passphrase" && return 1
     else
 	print_error "Unable to contact the resource manager" && return 1
@@ -150,81 +171,102 @@ unseal_passphrase() {
     return 0
 }
 
-is_luks_partition() {
+is_luks_volume() {
     cryptsetup isLuks "$1"
 }
 
-create_luks_partition() {
+create_luks_volume() {
     local luks_dev="$1"
     local luks_name="$2"
     local tpm_absent="$3"
     local tmp_dir="$4"
     local passphrase="-"
 
-    print_info "Creating the LUKS partition $luks_name ..."
+    print_info "Creating the LUKS volume \"$luks_name\" ..."
 
     if [ "$tpm_absent" = "0" ]; then
-        ! cryptfs-tpm2 -q unseal passphrase -P auto -o "$tmp_dir/passphrase" &&
-            print_error "Unable to unseal the passphrase" && return 1
+        local pcr_opt=""
 
-        passphrase="$tmp_dir/passphrase"
+        if [ $OPT_USE_PCR -eq 1 ]; then
+            pcr_opt="-P auto"
+        fi
+
+        tpm2_getcap handles-persistent | grep -q 0x817FFFFF &&
+            tpm2_getcap handles-persistent | grep -q 0x817FFFFE && {
+                ! cryptfs-tpm2 -q unseal passphrase $pcr_opt -o "$tmp_dir/passphrase" &&
+                    print_error "Unable to unseal the passphrase" && return 1;
+
+                passphrase="$tmp_dir/passphrase"
+            } || {
+                print_warning "Not found the sealed passphrase from TPM"
+
+                # Prevent from a single primary key or passphrase remained
+                cryptfs-tpm2 -q evict all >/dev/null 2>&1 || true
+
+                print_info "Automatically sealing the passphrase to TPM ..."
+
+                ! cryptfs-tpm2 -q seal all $pcr_opt &&
+                    print_error "Unable to seal the primary key and passphrase" &&
+                    exit 1
+            }
     fi
 
     ! cryptsetup --type luks --cipher aes-xts-plain --hash sha256 \
         --use-random --key-file "$passphrase" luksFormat "$luks_dev" &&
-        print_error "Unable to create the LUKS partition on $luks_dev" &&
+        print_error "Unable to create the LUKS volume on $luks_dev" &&
         return 1
 
     return 0
 }
 
-map_luks_partition() {
+map_luks_volume() {
     local luks_dev="$1"
     local luks_name="$2"
     local tpm_absent="$3"
     local tmp_dir="$4"
     local passphrase="-"
 
-    print_info "Mapping the LUKS partition $luks_name ..."
+    print_info "Mapping the LUKS volume \"$luks_name\" ..."
 
     [ "$tpm_absent" = "0" ] && passphrase="$4/passphrase"
 
     ! cryptsetup luksOpen --key-file "$passphrase" "$luks_dev" "$luks_name" &&
-        print_error "Unable to map the LUKS partition $luks_name" && return 1
+        print_error "Unable to map the LUKS volume $luks_name" && return 1
 
     return 0
 }
 
-unmap_luks_partition() {
+unmap_luks_volume() {
     local luks_name="$1"
 
-    [ -e "/dev/mapper/$luks_name" ] && print_info "Unmapping the LUKS partition $luks_name ..." &&
-        cryptsetup luksClose "$luks_name"
+    [ -e "/dev/mapper/$luks_name" ] && print_info "Unmapping the LUKS volume $luks_name ..." &&
+        cryptsetup luksClose "$luks_name" || true
 }
 
 option_check() {
     if [ -z "$1" ] || echo "$1" | grep -q "^-"; then
-        print_error "No value specified for option $arg"
+        print_error "No value specified for the option $arg"
         exit 1
     fi
 }
 
 show_help() {
     cat <<EOF
-$PROG_NAME - creation tool for LUKS partition
+$PROG_NAME - creation tool for LUKS volume
 Version: $VERSION
 
-(C)Copyright 2016-2017, Wind River Systems, Inc.
+(C)Copyright 2024, Alibaba Cloud
+(C)Copyright 2016-2023, Wind River Systems, Inc.
 
 Usage: $1 options...
 
 Options:
  -d|--dev <device>
-     Set the path to the device node where the LUKS partition is manipulated.
+     Set the path to the backing device where the LUKS volume is manipulated.
      e.g, /dev/sda1
 
  -N|--no-setup
-     In no setup mode you can map/unmap existing LUKS partitions.
+     In this mode you can map/unmap existing LUKS volume.
 
  -n|--name <mapped name>
     (Optional) Set the mapped name for the dmcrypt target device.
@@ -233,18 +275,21 @@ Options:
     The default name is $DEFAULT_ENCRYPTION_NAME.
 
  -f|--force
-    (Optional) Enforce creating the LUKS partition if already existed.
+    (Optional) Enforce formating the LUKS volume if already existed.
 
  -m|--map-existing
-    (Optional) Map previously created LUKS partition.
+    (Optional) Map previously created LUKS volume.
 
  -u|--unmap
-    (Optional) Unmap the created LUKS partition before exiting.
+    (Optional) Unmap the created LUKS volume before exiting.
 
  -t|--no-tpm
     (Optional) Don't use TPM.
 
     By default, TPM is probed automatically.
+
+ -p|--use-pcr
+    (Optional) Use PCR to seal/unseal the primary key and passphrase.
 
  -e|--evict-all
     (Optional) Always evict the primary key and passphrase.
@@ -272,8 +317,10 @@ PROG_NAME=`basename $0`
 OPT_FORCE_CREATION=0
 OPT_UNMAP_LUKS=0
 OPT_NO_TPM=0
+OPT_USE_PCR=0
 OPT_EVICT_ALL=0
 OPT_VERBOSE=0
+# Depreciated
 OPT_MAP_EXISTING_LUKS=0
 OPT_NO_SETUP=0
 OPT_OLD_LOCKOUT_AUTH=""
@@ -303,6 +350,9 @@ while [ $# -gt 0 ]; do
 	-t|--no-tpm)
 	    OPT_NO_TPM=1
 	    ;;
+	-p|--use-pcr)
+	    OPT_USE_PCR=1
+	    ;;
 	-e|--evict-all)
 	    OPT_EVICT_ALL=1
 	    ;;
@@ -331,13 +381,13 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-trap "trap_handler $?" SIGINT EXIT
+trap 'trap_handler $LINENO $?' SIGINT EXIT ERR
 
 OPT_LUKS_NAME="${OPT_LUKS_NAME:-$DEFAULT_ENCRYPTION_NAME}"
 
-###### OPT_NO_SETUP Handling start ######
+#### Handle OPT_NO_SETUP=1 ####
 if [ $OPT_NO_SETUP -eq 1 ] && [ $OPT_UNMAP_LUKS -eq 1 ] ; then
-    unmap_luks_partition "$OPT_LUKS_NAME"
+    unmap_luks_volume "$OPT_LUKS_NAME"
     exit 0
 fi
 
@@ -350,34 +400,32 @@ print_verbose "Temporary directory created: $TEMP_DIR"
     exit 1
 
 if [ $OPT_NO_SETUP -eq 1 ] ; then
-    if [ $OPT_MAP_EXISTING_LUKS -eq 1 ]; then
-	if ! is_luks_partition "$OPT_LUKS_DEV"; then
-            print_info "$OPT_LUKS_DEV is not a LUKS partition"
-            exit 1
-	fi
-
-	if [ $OPT_NO_TPM -eq 0 ] ; then
-	    detect_tpm
-	    [ $? -eq 0 ] && {
-                TPM_ABSENT=0
-                ! unseal_passphrase "$TEMP_DIR/passphrase" && exit 1
-            }
-	fi
-
-	! map_luks_partition "$OPT_LUKS_DEV" "$OPT_LUKS_NAME" \
-            "$TPM_ABSENT" "$TEMP_DIR" && exit 1
-
-	print_info "The LUKS partition $OPT_LUKS_NAME is created on $OPT_LUKS_DEV"
+    if ! is_luks_volume "$OPT_LUKS_DEV"; then
+        print_info "$OPT_LUKS_DEV is not a LUKS volume"
+        exit 1
     fi
+
+    if [ $OPT_NO_TPM -eq 0 ] ; then
+	detect_tpm
+	[ $? -eq 0 ] && {
+            TPM_ABSENT=0
+            ! unseal_passphrase "$TEMP_DIR/passphrase" && exit 1
+        }
+    fi
+
+    ! map_luks_volume "$OPT_LUKS_DEV" "$OPT_LUKS_NAME" \
+        "$TPM_ABSENT" "$TEMP_DIR" && exit 1
+
+    print_info "The LUKS volume \"$OPT_LUKS_NAME\" backing on $OPT_LUKS_DEV is created"
     exit 0
 fi
+#### End of handling OPT_NO_SETUP=1 ####
 
-###### OPT_NO_SETUP Handling end ######
-if is_luks_partition "$OPT_LUKS_DEV"; then
-    print_info "$OPT_LUKS_DEV is already a LUKS partition"
+if is_luks_volume "$OPT_LUKS_DEV"; then
+    print_info "$OPT_LUKS_DEV is already a LUKS volume. Politely exit"
     [ $OPT_FORCE_CREATION -eq 0 ] && exit 0
 
-    print_info "Enforce creating the LUKS partition"
+    print_info "Enforce creating the LUKS volume"
 fi
 
 echo
@@ -402,9 +450,8 @@ if [ $OPT_NO_TPM -eq 0 ]; then
     detect_tpm
     if [ $? -eq 0 ]; then
         if [ $OPT_EVICT_ALL -eq 1 ]; then
-            cmd="tpm2_clear"
-            [ -n "$OPT_OLD_LOCKOUT_AUTH" ] && cmd="${cmd} --oldLockPasswd $OPT_OLD_LOCKOUT_AUTH"
-            [ -n "$OPT_LOCKOUT_AUTH" ] && cmd="${cmd} --LockPasswd $OPT_LOCKOUT_AUTH"
+            cmd="tpm2_changeauth -c lockout"
+            [ -n "$OPT_OLD_LOCKOUT_AUTH" ] && cmd="${cmd} --object-auth=$OPT_OLD_LOCKOUT_AUTH"
             eval "$cmd"
             if [ $? -ne 0 ]; then
                 print_error "Failed to clear authorization values with the lockoutAuth specified"
@@ -416,14 +463,21 @@ if [ $OPT_NO_TPM -eq 0 ]; then
             # TPM2_Startup).
             cmd="tpm2_dictionarylockout --setup-parameters --max-tries 1 \
                      --recovery-time 0 --lockout-recovery-time 0"
-            [ -n "$OPT_LOCKOUT_AUTH" ] && cmd="${cmd} --lockout-passwd $OPT_LOCKOUT_AUTH"
+            [ -n "$OPT_LOCKOUT_AUTH" ] && cmd="${cmd} $OPT_LOCKOUT_AUTH"
             eval "$cmd"
             if [ $? -ne 0 ]; then
                 print_error "Failed to set the default DA policy"
                 exit 1
             fi
 
-            ! cryptfs-tpm2 -q seal all -P auto &&
+            cryptfs-tpm2 -q evict all >/dev/null 2>&1 || true
+
+	    local pcr_opt=""
+	    if [ $OPT_USE_PCR -eq 1 ]; then
+                pcr_opt="-P auto"
+	    fi
+
+            ! cryptfs-tpm2 -q seal all $pcr_opt &&
                 print_error "Unable to create the primary key and passphrase" &&
                 exit 1
         fi
@@ -442,12 +496,12 @@ if [ $TPM_ABSENT = 1 ]; then
     echo
 fi
 
-! create_luks_partition "$OPT_LUKS_DEV" \
+! create_luks_volume "$OPT_LUKS_DEV" \
     "$OPT_LUKS_NAME" "$TPM_ABSENT" "$TEMP_DIR" && exit 1
 
-! map_luks_partition "$OPT_LUKS_DEV" "$OPT_LUKS_NAME" \
+! map_luks_volume "$OPT_LUKS_DEV" "$OPT_LUKS_NAME" \
     "$TPM_ABSENT" "$TEMP_DIR" && exit 1
 
-[ $OPT_UNMAP_LUKS -eq 1 ] && unmap_luks_partition "$OPT_LUKS_NAME"
+[ $OPT_UNMAP_LUKS -eq 1 ] && unmap_luks_volume "$OPT_LUKS_NAME"
 
-print_info "The LUKS partition $OPT_LUKS_NAME is created on $OPT_LUKS_DEV"
+print_info "The LUKS volume $OPT_LUKS_NAME is created on $OPT_LUKS_DEV"
