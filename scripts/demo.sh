6383cc8232034deac7fbed4f6f01c7976b5ff237
Jia Zhang <zhang.jia@linux.alibaba.com>
2024-05-17 11:04:25 +0800

    demo.sh: support to use non-tpm to generate the LUKS passphrase
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -14,3 +14,8 @@ run "luks-setup --dev /dev/nvme2n1p1 --force"
 run "lsblk"
 run "luks-setup --unmap"
 run "lsblk"
+
+run "luks-setup --dev /dev/nvme2n1p1 --force --no-tpm"
+run "lsblk"
+run "luks-setup --unmap"
+run "lsblk"
