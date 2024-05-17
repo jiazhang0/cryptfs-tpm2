35486726f20befb8c29256cbb7c726e6f15c8cd5
Jia Zhang <zhang.jia@linux.alibaba.com>
2024-05-17 09:36:06 +0800

    Add the new script demo.sh
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

---
+++ scripts/demo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+luks-setup --unmap
+rpm -e cryptsetup
+clear
+
+run() {
+    local cmd="$1"
+    echo "# $cmd"
+    eval "$cmd"
+}
+
+run "luks-setup --dev /dev/nvme2n1p1 --force"
+run "lsblk"
+run "luks-setup --unmap"
+run "lsblk"
